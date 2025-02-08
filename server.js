aacb0c26125e9bf9f8e96cdb77c186a62ebbd60d
Helena Żabska <helenazabska@macbook-pro-helena.home>
2025-02-08 13:26:16 +0100

    starting page

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 
 app.use(express.urlencoded({ extended: true })); 
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, "public"))); 
 app.use(session({
     secret: "babu",
