cd666cd5cfaf4cf1ac254f38f67601b64aaef2c8
rpecherz <150146663+rpecherz@users.noreply.github.com>
2025-02-08 09:23:47 +0100

    Revert "Database added"

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-# WebShop
-
-## Running locally
-
-### Create local database
-
-Make sure you have postgresql installed.
-
-Run these commands:
-
-psql -d posgres
-
-Now inside psql command line:
-
-\i /your/path/create_db.sql
-
-Remember to change database credentials (especially user) in db/db.js lines 3-9.
+# webshop
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
-const client = require("../db/db");
+
 
 function admid(req, res, next) {
     if (!req.session || !req.session.user || req.session.user.role !== "admin") {
@@ -10,17 +10,12 @@ function admid(req, res, next) {
     next();
 }
 
+// Dane testowe
+let products = [];
+
 // Panel administratora
 router.get("/", admid, (req, res) => {
-    client.query("SELECT * FROM products")
-        .then(result => {
-            const products = result.rows;
-            res.render("admin", { users: [], products, orders: [] });
-        })
-        .catch(err => {
-            console.error('Database query error', err.stack);
-            res.status(500).send('Database query error');
-        });
+    res.render("admin", { users: [], products, orders: [] });
 });
 
 router.post("/products/add", [
@@ -31,16 +26,8 @@ router.post("/products/add", [
     if (!errors.isEmpty()) {
         return res.status(400).send("Błąd walidacji");
     }
-
-    const { name, price } = req.body;
-    client.query('INSERT INTO products (name, price) VALUES ($1, $2)', [name, price])
-        .then(() => {
-            res.redirect("/admin");
-        })
-        .catch(err => {
-            console.error('Database insert error', err.stack);
-            res.status(500).send('Error adding product');
-        });
+    products.push({ id: products.length + 1, name: req.body.name, price: req.body.price });
+    res.redirect("/admin");
 });
 
 module.exports = router;
